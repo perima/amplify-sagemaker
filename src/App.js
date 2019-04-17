c27288e45773f2af905ae9abf9a2043db0ecb78c
EC2 Default User <ec2-user@ip-172-31-21-174.ap-southeast-1.compute.internal>
2019-04-17 07:30:39 +0000

    add api and lambda for sagemaker invocation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Header, Button, Checkbox, Form, Grid, Container } from 'semantic-ui-react'
+import axios from 'axios';
+
+/*
+
+by the way for the UI .. i need a submit button and 6 text boxes with options 
+buying, maintained, doors, persons, luggage_boot, and safety . We can finish it in next few weeks -
+
+*/
+
 
 class App extends Component {
+  
+  constructor(props){
+    super(props)
+    
+    this.state = {
+      inferenceURL: null
+    }
+    
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+    handleChange(event) {
+      console.log(event.target.value);
+        this.setState({inferenceURL: event.target.value});
+      }
+
+    handleInference(event) {
+      console.log('handleInference: ', event.target.value);
+      }
+    
+  handleInferenceOld = event => {
+    event.preventDefault();
+
+    const payload = {
+        
+      };
+      
+      console.log('handleInference', payload);
+
+    /*
+    axios.post(`https://jsonplaceholder.typicode.com/users`, { payload })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      });
+      
+       value: {this.state.inferenceURL}
+       
+      */
+  }
+  
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div>
+        <Container text>
+          <Header as='h1'>From Inception to Inference</Header>
+            <Form>
+              <Form.Field>
+                <label>Inference URL</label>
+                <input
+                name="name"
+                  placeholder='Deployed model inference endpoint' 
+                  onChange={this.handleChange} 
+                  value={this.state.inferenceURL}
+                />
+              </Form.Field>
+              <Form.Field>
+                <label>Buying</label>
+                <input placeholder='buying' />
+              </Form.Field>
+                    <Form.Field>
+                      <label>Maintained</label>
+                      <input placeholder='maintained' />
+                    </Form.Field>
+                    <Form.Field>
+                      <label>Doors</label>
+                      <input placeholder='doors' />
+                    </Form.Field>
+                    <Form.Field>
+                      <label>Persons</label>
+                      <input placeholder='persons' />
+                    </Form.Field>
+                    <Form.Field>
+                      <label>Luggage boot</label>
+                      <input placeholder='luggage boot' />
+                    </Form.Field>
+                    <Form.Field>
+                      <label>Safety</label>
+                      <input placeholder='safety' />
+                    </Form.Field>
+                    <Button onClick={this.handleInference}>Get inference</Button>
+                  </Form>
+            </Container>
+            </div>
     );
   }
 }
